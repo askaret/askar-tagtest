7ee051ba5da6356610213d308446c74ed5dc1c77
Andreas Skaret <andreas.skaret@gmail.com>
2020-10-27 13:23:24 +0100

    Update Request-PreRelease.ps1

--- scripts/Request-PreRelease.ps1
+++ scripts/Request-PreRelease.ps1
@@ -5,7 +5,6 @@ if ($args.count -ne 1) {
 
 $branch = $args[0].Split("/")
 $gitTag = $branch[$branch.count - 1]
-$gitBranch = git branch --contains $gitTag
-
+$gitBranch = git branch -a --contains $gitTag
 Write-Host "Creating new PRE-Release from tag $($gitTag)"
 Write-Host "Guessing $($gitBranch) is the git branch"
